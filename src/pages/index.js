0e080b5c8b25c35571bcf7cdd697c73c5a63ff21
Simon Stratton <simon.stratton@safebear.co.uk>
2021-01-20 20:43:05 +0000

    updating background images

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import SlideShow from '../components/SlideShow';
 
 var settings = {
   images: [
-    { url: require('../assets/images/bg01.jpg'), position: 'center' },
-    { url: require('../assets/images/bg02.jpg'), position: 'center' },
-    { url: require('../assets/images/bg03.jpg'), position: 'center' },
+    { url: require('../assets/images/sbimage1.jpg'), position: 'center' },
+    { url: require('../assets/images/sbimage2.jpg'), position: 'center' },
+    { url: require('../assets/images/sbimage3.jpg'), position: 'center' },
   ],
   // Delay.
   delay: 6000,
